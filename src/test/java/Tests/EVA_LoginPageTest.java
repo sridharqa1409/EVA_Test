411652caeba4e5ae465e8183e3ebb182ab55cdbc
sridh <sridh@DESKTOP-VC0C0A9>
2020-02-21 15:43:47 +0530

    alligned

--- src/test/java/Tests/EVA_LoginPageTest.java
+++ src/test/java/Tests/EVA_LoginPageTest.java
@@ -10,56 +10,50 @@ import org.testng.annotations.Test;
 import Pages.EVA_LoginPage;
 import TestBase.TestBase;
 
-public class EVA_LoginPageTest extends TestBase{
+public class EVA_LoginPageTest extends TestBase {
 
 	EVA_LoginPage LoginPage;
-	
-	
+
 	@BeforeMethod
 	public void SetUp() {
 
 		initBrowserAndGotoUrl("https://aq5z5o4vcj.execute-api.ap-south-1.amazonaws.com/production/login");
-		LoginPage = new EVA_LoginPage(); 
+		LoginPage = new EVA_LoginPage();
 	}
-	
-	
+
 	@DataProvider
-	public Object[][] getTestData(){
-		
-		Object obj[][] =new Object[4][2];
-		
-		obj[0][0]="test@fastcollab.com";
-		obj[0][1]="Myadmin@123";
-		
-		obj[1][0]="sridhar.moddu@fastcollab.com";
-		obj[1][1]="myadmin@123";
-		
-		obj[2][0]="test@fastcollab.com";
-		obj[2][1]="admin@123";
-		
-		obj[3][0]="sridhar.moddu@fastcollab.com";
-		obj[3][1]="Myadmin@123";
-		
+	public Object[][] getTestData() {
+
+		Object obj[][] = new Object[4][2];
+
+		obj[0][0] = "test@fastcollab.com";
+		obj[0][1] = "Myadmin@123";
+
+		obj[1][0] = "sridhar.moddu@fastcollab.com";
+		obj[1][1] = "myadmin@123";
+
+		obj[2][0] = "test@fastcollab.com";
+		obj[2][1] = "admin@123";
+
+		obj[3][0] = "sridhar.moddu@fastcollab.com";
+		obj[3][1] = "Myadmin@123";
+
 		return obj;
-			
+
 	}
-	
-	
-	
-	@Test(priority=1,dataProvider = "getTestData")
-	public void loginTest(String Username,String Password) {
-		
-		LoginPage.login(Username,Password);
-			
+
+	@Test(priority = 1, dataProvider = "getTestData")
+	public void loginTest(String Username, String Password) {
+
+		LoginPage.login(Username, Password);
+
 	}
-	
-	
+
 	@AfterMethod
 	public void teardown() {
-		
+
 		driver.close();
-		
+
 	}
-	
-	
+
 }
