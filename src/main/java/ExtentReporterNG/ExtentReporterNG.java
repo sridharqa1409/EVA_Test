9764665499da94938dc5c56938c3a9378d77d0c7
sridh <sridh@DESKTOP-VC0C0A9>
2020-03-01 23:14:56 +0530

    Extent

--- src/main/java/ExtentReporterNG/ExtentReporterNG.java
+++ src/main/java/ExtentReporterNG/ExtentReporterNG.java
@@ -22,7 +22,7 @@ public class ExtentReporterNG implements IReporter {
 	private ExtentReports extent;
 
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
-		extent = new ExtentReports(outputDirectory + File.separator + "Extent.html", true);
+		extent = new ExtentReports(outputDirectory + File.separator + "EVA_ExtentReport.html", true);
 
 		for (ISuite suite : suites) {
 			Map<String, ISuiteResult> result = suite.getResults();
