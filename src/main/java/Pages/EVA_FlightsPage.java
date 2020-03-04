58abfb7284bc0386a4cf96aff8be1969a6e2f5f1
sridh <sridh@DESKTOP-QNMRRVK>
2020-03-04 11:25:25 +0530

    REfesh

--- src/main/java/Pages/EVA_FlightsPage.java
+++ src/main/java/Pages/EVA_FlightsPage.java
@@ -6,7 +6,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 import TestBase.TestBase;
